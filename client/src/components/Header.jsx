2e5199b3d38e5fa1bd1f69e99a4a0548ff6eca7e
Wilfred Muchire <logincoin40@gmail.com>
2024-07-02 11:49:32 +0300

    Adding the header section with the navigations

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,25 +1,52 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import './header.css'
+import logo from '../assets/react.svg'
 
 const Header = () => {
     return (
-      <div>
+      <div className="headerSect">
+        <div className="header-logo">
+          <div className="logo_img">
+            <img src={logo} alt="" />
+          </div>
+          <h1>Great tutors</h1>
+        </div>
         <nav>
           <ul>
             <li>
-              <Link to={"/"}> Home</Link>
+              <Link className="link" to={"/"}>
+                {" "}
+                Home
+              </Link>
             </li>
             <li>
-              <Link to={"/degrees"}>Degrees</Link>
+              <Link className="link" to={"/degrees"}>
+                Degrees
+              </Link>
             </li>
             <li>
-              <Link to={"/about_us"}>About us</Link>
+              <Link className="link" to={"/about_us"}>
+                About us
+              </Link>
             </li>
             <li>
-              <Link to={"/contact_us"}>Contact us</Link>
+              <Link className="link" to={"/contact_us"}>
+                Contact us
+              </Link>
             </li>
           </ul>
         </nav>
+
+        <div className="signUp_signIn">
+          <a href="#">
+            <button>Sing in </button>
+          </a>
+
+          <a href="#">
+            <button>Sing up</button>
+          </a>
+        </div>
       </div>
     );
 }
