3c142f5aa22ffd75e4c2ec8f1ae2fe4104594c28
Wilfred Muchire <logincoin40@gmail.com>
2024-07-02 16:04:29 +0300

    Adding the top courses sections together with its syling

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -3,11 +3,13 @@ import Heros from './Heros';
 import Banner from './Banner';
 import PopularCourses from './PopularCourses';
 import './home.css'
+import TopCourses from './TopCourses';
 const Home = () => {
     return <div>
         <Heros/>
         <Banner/>
         <PopularCourses/>
+        <TopCourses/>
     </div>;
 }
 
--- client/src/pages/Home/TopCourses.jsx
+++ client/src/pages/Home/TopCourses.jsx
@@ -1,10 +1,59 @@
 import React from 'react';
-const TopCourses = () => {
-    return <div></div>;
+import './home.css'
+import ViewMore from '../../components/ViewMore'
+import SectionTitle from '../../components/SectionTitle'
+import top_courses from '../../data/top_courses';
+const Top_courses = ({course_img, course_category, course_name, course_desc, course_duration}) => {
+    return (
+      <>
+        <div className="top_course">
+          <div className="top_course_img">
+            <img src={course_img} alt="Top course Image" />
+          </div>
+          <div className="about_course">
+            <div className="top">
+              <p className="cartegory">{course_category}</p>
+              <div className="ratings">
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star checked"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+              </div>
+            </div>
+            <h2>{course_name}</h2>
+            <p className="desc">{course_desc}</p>
+            <p className="duration">{course_duration}</p>
+          </div>
+          <div className="bottom">
+            <p>More Infor</p>
+            <button>Apply</button>
+          </div>
+        </div>
+      </>
+    );
 }
+const TopCourses = () => {
+    return (
+      <div>
+        <div className="top_couserseSect">
+          <SectionTitle title={"Top courses"} />
+          <div className="top_courses">
+            {top_courses.map((top_course, i) => (
+              <Top_courses
+                course_img={top_course.courseImg}
+                course_category={top_course.courseCartegory}
+                course_name={top_course.courseName}
+                course_desc={top_course.courseDesc}
+                course_duration={top_course.courseDuration}
+              />
+            ))}
+          </div>
 
-TopCourses.propTypes = propTypes;
-TopCourses.defaultProps = defaultProps;
-// #endregion
+          <ViewMore/>
+        </div>
+      </div>
+    );
+}
 
 export default TopCourses;
