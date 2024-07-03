58058c07640f3fd58fdbc37e129f33782eb4af02
Wilfred Muchire <logincoin40@gmail.com>
2024-07-03 15:42:31 +0300

    Adding the about degrees section

--- client/src/pages/Degrees/Degrees.jsx
+++ client/src/pages/Degrees/Degrees.jsx
@@ -1,10 +1,59 @@
 import React from "react";
 import "./degrees.css";
 import Banner from "../../components/Banner";
+import SectionTitle from '../../components/SectionTitle'
+
 const Degrees = () => {
-  return <div>
-    <Banner dg_title={'Apply and get to learn your preffered degree program with us'} dg_desc={'Get to learn with us and improve your career with our accreditted degree programs which are recognized world wide by different companies.'}/>
-  </div>;
+  return (
+    <div>
+      <Banner
+        dg_title={
+          "Apply and get to learn your preffered degree program with us"
+        }
+        dg_desc={
+          "Get to learn with us and improve your career with our accreditted degree programs which are recognized world wide by different companies."
+        }
+      />
+
+      <div className="degree_programs">
+        <SectionTitle title={"View some of our degree programs."} />
+        <div className="degree_desc">
+          <div className="dg_info">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur
+              officia ipsam tempore distinctio voluptatum corporis ullam? Modi
+              ratione vel pariatur repudiandae, ab culpa rem harum molestias
+              iste eveniet vero dolorum!
+            </p>
+          </div>
+          <div className="dg_info">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur
+              officia ipsam tempore distinctio voluptatum corporis ullam? Modi
+              ratione vel pariatur repudiandae, ab culpa rem harum molestias
+              iste eveniet vero dolorum!
+            </p>
+          </div>
+          <div className="dg_info">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur
+              officia ipsam tempore distinctio voluptatum corporis ullam? Modi
+              ratione vel pariatur repudiandae, ab culpa rem harum molestias
+              iste eveniet vero dolorum!
+            </p>
+          </div>
+          <div className="dg_info">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur
+              officia ipsam tempore distinctio voluptatum corporis ullam? Modi
+              ratione vel pariatur repudiandae, ab culpa rem harum molestias
+              iste eveniet vero dolorum!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Degrees;
