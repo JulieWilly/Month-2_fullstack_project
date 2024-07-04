40e6f255d2014452134900ab4d7055c40d4059bd
Wilfred Muchire <logincoin40@gmail.com>
2024-07-04 15:41:26 +0300

    Adding the post HTTP request to create a new user

--- server/src/routers/users.routers.js
+++ server/src/routers/users.routers.js
@@ -1,12 +1,35 @@
 import { Router } from "express";
 import {config} from "dotenv"
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 
 
 const router = Router();
 config();
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
+    try{
+        const {firstName, lastName, email, password} = req.body;
+        if (!firstName || !lastName || !email || !password) {
+            res.send({message:" Please enter all fields"})
+        }
+
+
+        const create_user = await prisma.user.create({
+            data:{
+                firstName,
+                lastName,
+                email,
+                password
+            }
+        })
+        res.status(200).json({success: true, data:create_user})
+    } catch(error) {
+        res.status(500).json({success:false, message: error.message})
+    }
+
     
 })
 
