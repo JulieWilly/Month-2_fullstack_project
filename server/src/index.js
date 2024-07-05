a7f160dba9f0ef304ebd28c3e03dec4d7d4017bc
Wilfred Muchire <logincoin40@gmail.com>
2024-07-05 12:27:32 +0300

    ADding the post http request

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,9 @@ import express from 'express'
 import userRouter from './routers/users.routers.js'
 
 
+
 const great_tutor = express();
+great_tutor.use(express.json())
 great_tutor.use("/greatTutor.org", userRouter)
 
 great_tutor.listen(3000, () => {
--- server/src/routers/users.routers.js
+++ server/src/routers/users.routers.js
@@ -8,10 +8,25 @@ const prisma = new PrismaClient()
 const router = Router();
 config();
 
-router.post('/login', (req, res) => {
-    // try{
-    //     const {firstName,lastName, email, password}
-    // }
+router.post('/login',async (req, res) => {
+    try{
+        const {firstName,lastName, email, password} = req.body
+
+        if (!firstName || !lastName || !email || !password) {
+            res.send("Please provide all the fields required.")
+        }
+        const createUser = await prisma.user.create({
+            data:{
+                firstName,lastName,email,password
+            }
+
+        })
+            res.status(200).json({success:true, message:'The user has been created successfully', data:createUser})
+
+
+    }catch(err) {
+        res.status(500).json({ success: false, message: err.message})
+    }
 })
 
 export default router;
